17df0d6f5a148b04a692fb78863f88a7e004a926
codprogdotcom <codprog321@gmail.com>
2023-01-27 09:18:38 +0530

    add complete and clear all todos functionality

--- 014-react-form-and-project/src/App.js
+++ 014-react-form-and-project/src/App.js
@@ -4,11 +4,26 @@ import Todo from "./Todo";
 import TodoInputForm from "./TodoInputForm";
 function App() {
     const [todos, setTodos] = useState([
-        { id: 1, todoName: "learn react", completed: false },
-        { id: 2, todoName: "learn javascript", completed: true },
-        { id: 3, todoName: "practice react", completed: false },
-        { id: 4, todoName: "create project", completed: false },
+        { id: "1", todoName: "learn react", completed: false },
+        { id: "2", todoName: "learn javascript", completed: true },
+        { id: "3", todoName: "practice react", completed: false },
+        { id: "4", todoName: "create project", completed: false },
     ]);
+
+    function toggleCompleted(id) {
+        // console.log(id);
+        // id == 2
+
+        setTodos((prevTodos) => {
+            return prevTodos.map((todoObj) => {
+                if (todoObj.id === id) {
+                    return { ...todoObj, completed: !todoObj.completed };
+                } else {
+                    return todoObj;
+                }
+            });
+        });
+    }
 
     function addTodo(newTodo) {
         setTodos((prevTodos) => [...prevTodos, newTodo]);
@@ -43,6 +58,10 @@ function App() {
             prevTodos.filter((prevTodos) => prevTodos.id !== id)
         );
     }
+
+    function clearTodos() {
+        setTodos([]);
+    }
     return (
         <>
             <h1 style={{ textAlign: "center" }}>Todo List App</h1>
@@ -53,10 +72,14 @@ function App() {
                         <Todo
                             {...todoObj}
                             deleteTodo={deleteTodo}
+                            toggleCompleted={toggleCompleted}
                             key={todoObj.id}
                         />
                     );
                 })}
+                <button onClick={clearTodos} disabled={todos.length === 0}>
+                    Clear All Todos
+                </button>
             </div>
         </>
     );
--- 014-react-form-and-project/src/Todo.js
+++ 014-react-form-and-project/src/Todo.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./Todo.css";
-function Todo({ todoName, id, deleteTodo, completed }) {
+function Todo({ todoName, id, deleteTodo, completed, toggleCompleted }) {
     return (
         <div className="todo">
-            <h3 className="todoName">{todoName}</h3>
+            <h3 className={`todoName ${completed ? "completed" : ""}`}>
+                {todoName}
+            </h3>
             <div className="button-wrapper">
-                <button>Complete</button>
+                <button
+                    onClick={() => {
+                        toggleCompleted(id);
+                    }}
+                >
+                    {completed ? "incomplete" : "complete"}
+                </button>
                 <button
                     onClick={() => {
                         deleteTodo(id);
