f67bd3ced2e767d76867019f06f5271e56fefc05
codprogdotcom <codprog321@gmail.com>
2023-02-04 09:43:51 +0530

    search movies using fetch

--- 024-movie-search-project/src/MovieSearch.jsx
+++ 024-movie-search-project/src/MovieSearch.jsx
@@ -2,8 +2,26 @@ import { useState } from "react";
 
 function MovieSearch() {
     const [movieTitle, setMovieTitle] = useState("");
+    async function searchMovies() {
+        const searchURL = `https://www.omdbapi.com/?s=${movieTitle}&apikey=${
+            import.meta.env.VITE_API_KEY
+        }`;
+        const response = await fetch(searchURL);
+        if (!response.ok) {
+            throw new Error(
+                `Something went Wrong, status code ${response.status}`
+            );
+        }
+        const moviesData = await response.json();
+        console.log(moviesData);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        searchMovies();
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input
                 type="text"
                 value={movieTitle}
@@ -15,3 +33,17 @@ function MovieSearch() {
 }
 
 export default MovieSearch;
+
+// javascript Engine --- synchronouns programming
+
+// whenever we want to do something in the background (like data fetching, setTimeout, setInterval)
+// we handover that work to broswer.
+
+// promise ---> future value
+
+// promise --> fetch data
+// pending , fullfiled , rejected
+
+// fetch()
+
+// broswer
