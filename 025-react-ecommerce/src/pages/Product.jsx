f8ce6a7c3651f8512dc25cc72cf7241f8324f22d
codprogdotcom <codprog321@gmail.com>
2023-02-06 09:20:41 +0530

    add use params in product component

--- 025-react-ecommerce/src/pages/Product.jsx
+++ 025-react-ecommerce/src/pages/Product.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-
 function Product() {
-    const { id } = useParams();
-
-    return (
-        <div>
-            Single Product
-            <p>id: {id}</p>
-        </div>
-    );
+    // path="/products/:id"
+    // url : https://localhost:5173/products/12
+    const { id } = useParams(); // {id: 12}
+    return <div>Single Product , id : {id}</div>;
 }
 
 export default Product;
