cfd13d8963aa3707550ba0c74677cda0d56c11b5
codprogdotcom <codprog321@gmail.com>
2023-02-06 08:58:10 +0530

    add root layout

--- 025-react-ecommerce/src/App.jsx
+++ 025-react-ecommerce/src/App.jsx
@@ -7,19 +7,12 @@ import {
 } from "react-router-dom";
 import About from "./pages/About";
 import Cart from "./pages/Cart";
-
+import RootLayout from "./layouts/RootLayout";
 import HomePage from "./pages/HomePage";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
-        <Route
-            element={
-                <>
-                    Navbar <Outlet />
-                </>
-            }
-            path="/"
-        >
+        <Route element={<RootLayout />} path="/">
             <Route index element={<HomePage />} />
             <Route path="/about" element={<About />} />
             <Route path="/cart" element={<Cart />} />
