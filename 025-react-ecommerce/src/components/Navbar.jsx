f077e7d9cd4a9ee49f47fd2f8020450359033fac
codprogdotcom <codprog321@gmail.com>
2023-02-06 09:47:58 +0530

    add nav links

--- 025-react-ecommerce/src/components/Navbar.jsx
+++ 025-react-ecommerce/src/components/Navbar.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <nav className="container nav">
+            <h2 className="logo">
+                <Link to="/">logo</Link>
+            </h2>
+            <ul className="nav-items">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/products">Products</Link>
+                </li>
+                <li>
+                    <Link to="/cart">Cart</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 }
 
 export default Navbar;
